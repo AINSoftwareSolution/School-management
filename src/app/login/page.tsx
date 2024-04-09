91ca65394ec9a2d0e0d4cdaa02767d1e607d4397
Naeema bargir <bargirn5@gmail.com>
2024-04-09 11:26:06 +0530

    login layout added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,29 @@
+"use client"
+import { Tabs, TabPane } from "../component"
+
 const Login = () => {
     return (
-        <></>
+        <div>
+            <div className="grid grid-cols-2">
+                <div>
+                    image
+                </div>
+                <div>
+                    <Tabs>
+                        <TabPane title="Student">
+                            <> student form</>
+                        </TabPane>
+                        <TabPane title="Parent">
+                            <> Parent form</>
+                        </TabPane>
+                        <TabPane title="Teacher">
+                            <>Teacher form</>
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
