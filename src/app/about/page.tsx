29e9f082a8074d39954ae222a6117309e5fb43c9
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2024-04-23 17:00:44 +0530

    home-page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,28 @@
+import { Container } from "../component"
+
+const About = () => {
+    return(
+        <Container>
+            <>
+            <div className="w-full h-auto banner">
+                <div className="grid grid-cols-2" >
+                    <div className="text-black ">
+                    <div></div>
+                    <h1>gfsdgkjvbhdvre</h1>
+                    <h5>adsjefdd</h5>
+
+                    
+                   
+
+
+
+                </div>
+                </div>
+            </div>
+            </>
+        </Container>
+    )
+
+}
+
+export default About
