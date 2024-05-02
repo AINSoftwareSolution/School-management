7869acf4b6289ab7b0f5688feb3bed81c4edbf95
Naeema bargir <bargirn5@gmail.com>
2024-05-02 18:19:15 +0530

    details form handled

--- src/app/container/confirmation.tsx
+++ src/app/container/confirmation.tsx
@@ -1,5 +1,5 @@
 
-const Confirmation = () => {
+const Confirmation = ({values}:any) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2">
             <div></div>
@@ -7,84 +7,84 @@ const Confirmation = () => {
                 <h4 className="font-[800] text-[1.2rem]">Application Details</h4>
 
                 <h2 className="font-[600] mt-2 text-blue-600">Student Details:</h2>
-                <label>First Name:</label>
-                <label>Middle Name:</label>
-                <label>Last Name:</label>
-                <label>Gender:</label>
-                <label>Date of Birth:</label>
-                <label>Nationality:</label>
-                <label>Mother Tongue:</label>
-                <label>Place of Birth:</label>
-                <label>Religion:</label>
-                <label>Category:</label>
-                <label>Identification Marks:</label>
+                <label>First Name: {values?.first_name}</label>
+                <label>Middle Name: {values?.first_name}</label>
+                <label>Last Name: {values?.first_name}</label>
+                <label>Gender: {values?.first_name}</label>
+                <label>Date of Birth: {values?.first_name}</label>
+                <label>Nationality: {values?.first_name}</label>
+                <label>Mother Tongue: {values?.first_name}</label>
+                <label>Place of Birth: {values?.first_name}</label>
+                <label>Religion: {values?.first_name}</label>
+                <label>Category: {values?.first_name}</label>
+                <label>Identification Marks: {values?.first_name}</label>
 
 
                 <h2 className="font-[600] mt-2 text-blue-600">Medical Details:</h2>
-                <label>Blood Group:</label>
-                <label>Is your child differently abled: Yes</label>
-                <label>Remarks:</label>
+                <label>Blood Group: {values?.first_name}</label>
+                <label>Is your child differently abled: {values?.first_name}</label>
+                <label>Remarks: {values?.first_name}</label>
 
                 <h4 className="mt-4 font-[800] text-[1.2rem]">Contact Details</h4>
                 <h2 className="font-[600] mt-2 text-blue-600">Father Contact Details:</h2>
-                <label>First Name:</label>
-                <label>Middle Name:</label>
-                <label>Last Name:</label>
-                <label>Primary Contact No.:</label>
-                <label>Secondary Contact No.:</label>
-                <label>Email:</label>
-                <label>Date of Birth:</label>
-                <label>Education:</label>
-                <label>Occupation:</label>
-                <label>organization:</label>
-                <label>Designation:</label>
-                <label>Annual Income:</label>
+                <label>First Name: {values?.first_name}</label>
+                <label>Middle Name: {values?.first_name}</label>
+                <label>Last Name: {values?.first_name}</label>
+                <label>Primary Contact No.: {values?.first_name}</label>
+                <label>Secondary Contact No.: {values?.first_name}</label>
+                <label>Email: {values?.first_name}</label>
+                <label>Date of Birth: {values?.first_name}</label>
+                <label>Education: {values?.first_name}</label>
+                <label>Occupation: {values?.first_name}</label>
+                <label>organization: {values?.first_name}</label>
+                <label>Designation: {values?.first_name}</label>
+                <label>Annual Income: {values?.first_name}</label>
 
                 <h2 className="font-[600] mt-2 text-blue-600">Mother Contact Details:</h2>
-                <label>First Name:</label>
-                <label>Middle Name:</label>
-                <label>Last Name:</label>
-                <label>Primary Contact No.:</label>
-                <label>Secondary Contact No.:</label>
-                <label>Email:</label>
-                <label>Date of Birth:</label>
-                <label>Education:</label>
-                <label>Occupation:</label>
-                <label>organization:</label>
-                <label>Designation:</label>
-                <label>Annual Income:</label>
+                <label>First Name: {values?.first_name}</label>
+                <label>Middle Name: {values?.first_name}</label>
+                <label>Last Name: {values?.first_name}</label>
+                <label>Primary Contact No.: {values?.first_name}</label>
+                <label>Secondary Contact No.: {values?.first_name}</label>
+                <label>Email: {values?.first_name}</label>
+                <label>Date of Birth: {values?.first_name}</label>
+                <label>Education: {values?.first_name}</label>
+                <label>Occupation: {values?.first_name}</label>
+                <label>organization: {values?.first_name}</label>
+                <label>Designation: {values?.first_name}</label>
+                <label>Annual Income: {values?.first_name}</label>
 
                 <h2 className="font-[600] mt-2 text-blue-600">Address:</h2>
-                <label>Address Line 1:</label>
-                <label>Address Line 2:</label>
-                <label>Country:</label>
-                <label>State:</label>
-                <label>City:</label>
-                <label>PIN code:</label>
+                <label>Address Line 1: {values?.first_name}</label>
+                <label>Address Line 2: {values?.first_name}</label>
+                <label>Country: {values?.first_name}</label>
+                <label>State: {values?.first_name}</label>
+                <label>City: {values?.first_name}</label>
+                <label>PIN code: {values?.first_name}</label>
 
                 <h2 className="font-[600] mt-2 text-blue-600">Local Guardian Details :</h2>
-                <label>First Name:</label>
-                <label>Middle Name:</label>
-                <label>Last Name:</label>
-                <label>Relation:</label>
-                <label>Primary Contact No.:</label>
-                <label>Secondary Contact No.:</label>
-                <label>Email:</label>
+                <label>First Name: {values?.first_name}</label>
+                <label>Middle Name: {values?.first_name}</label>
+                <label>Last Name: {values?.first_name}</label>
+                <label>Relation: {values?.first_name}</label>
+                <label>Primary Contact No.: {values?.first_name}</label>
+                <label>Secondary Contact No.: {values?.first_name}</label>
+                <label>Email: {values?.first_name}</label>
 
                 <h4 className="mt-4 font-[800] text-[1.2rem]">Acadamic Details</h4>
                 <h2 className="font-[600] mt-2 text-blue-600">School Details (Applying for)</h2>
-                <label>Session: 2024-2025</label>
-                <label>Class:</label>
-                <label>Medium:</label>
-                <label>Second Language:</label>
+                <label>Session: {values?.first_name}</label>
+                <label>Class: {values?.first_name}</label>
+                <label>Medium: {values?.first_name}</label>
+                <label>Second Language: {values?.first_name}</label>
 
                 <h2 className="font-[600] mt-2 text-blue-600">Previous School Details:</h2>
-                <label>Previous School Name:</label>
-                <label>Board:</label>
-                <label>Passing out Class:</label>
-                <label>Percentage Achieved (%):</label>
-                <label>Year of Passing:</label>
-                <label>Reason for Withdrawal:</label>
+                <label>Previous School Name: {values?.first_name}</label>
+                <label>Board: {values?.first_name}</label>
+                <label>Passing out Class: {values?.first_name}</label>
+                <label>Percentage Achieved (%): {values?.first_name}</label>
+                <label>Year of Passing: {values?.first_name}</label>
+                <label>Reason for Withdrawal: {values?.first_name}</label>
             </div>
 
         </div>
--- src/app/container/contactDetails.tsx
+++ src/app/container/contactDetails.tsx
@@ -1,6 +1,6 @@
 import { InputField } from "../component";
 
-const ContactDetails = () => {
+const ContactDetails:React.FC<any> = ({ values, errors, handleChange }) => {
     return (
         <form className="space-y-4 md:space-y-6">
             <div className="grid grid-col-1 md:grid-cols-4 gap-4">
@@ -8,61 +8,61 @@ const ContactDetails = () => {
                 <div></div>
                 <div></div>
                 <div></div>
-                <InputField label="First Name" name={"father_first_name"} />
-                <InputField label="Middle Name" name={"father_middle_name"} />
-                <InputField label="Last Name" name={"father_last_name"} />
-                <InputField label="Email" name={"father_email"} />
-                <InputField label="Date of Birth" type="date" name={"father_dob"} />
-                <InputField label="Education" name={"father_education"} />
-                <InputField label="Occupation" name={"father-occupation"} />
-                <InputField label="organization" name={"father_organization"} />
-                <InputField label="Designation" name={"father_designation"} />
-                <InputField label="Annual Income" type="number" name={"father_annual_income"} />
-                <InputField label="Primary Contact No." name={"father_primary_contact_no"} />
-                <InputField label="Secondary Contact No." name={"father_secondary_contact_no"} />
+                <InputField label="First Name" name={"father_first_name"} value={values?.father_first_name} onChange={handleChange} error={errors.father_first_name}/>
+                <InputField label="Middle Name" name={"father_middle_name"} value={values?.father_middle_name} onChange={handleChange} error={errors.father_middle_name}/>
+                <InputField label="Last Name" name={"father_last_name"} value={values?.father_last_name} onChange={handleChange} error={errors.father_last_name}/>
+                <InputField label="Email" name={"father_email"} value={values?.father_email} onChange={handleChange} error={errors.father_email}/>
+                <InputField label="Date of Birth" type="date" name={"father_dob"} value={values?.father_dob} onChange={handleChange} error={errors.father_dob}/>
+                <InputField label="Education" name={"father_education"} value={values?.father_education} onChange={handleChange} error={errors.father_education}/>
+                <InputField label="Occupation" name={"father_occupation"} value={values?.father_occupation} onChange={handleChange} error={errors.father_occupation}/>
+                <InputField label="organization" name={"father_organization"} value={values?.father_organization} onChange={handleChange} error={errors.father_organization}/>
+                <InputField label="Designation" name={"father_designation"} value={values?.father_designation} onChange={handleChange} error={errors.father_designation}/>
+                <InputField label="Annual Income" type="number" name={"father_annual_income"} value={values?.father_annual_income} onChange={handleChange} error={errors.father_annual_income}/>
+                <InputField label="Primary Contact No." name={"father_primary_contact_no"} value={values?.father_primary_contact_no} onChange={handleChange} error={errors.father_primary_contact_no}/>
+                <InputField label="Secondary Contact No." name={"father_secondary_contact_no"} value={values?.father_secondary_contact_no} onChange={handleChange} error={errors.father_secondary_contact_no}/>
                
 
                 <h6 className="font-[600]">Mother Contact Details</h6>
                 <div></div>
                 <div></div>
                 <div></div>
-                <InputField label="First Name" name={"mother_first_name"} />
-                <InputField label="Middle Name" name={"mother_middle_name"} />
-                <InputField label="Last Name" name={"mother_last_name"} />
-                <InputField label="Email" name={"mother_email"} />
-                <InputField label="Date of Birth" type="date" name={"mother_dob"} />
-                <InputField label="Education" name={"mother_education"} />
-                <InputField label="Occupation" name={"father-occupation"} />
-                <InputField label="organization" name={"mother_organization"} />
-                <InputField label="Designation" name={"mother_designation"} />
-                <InputField label="Annual Income" type="number" name={"mother_annual_income"} />
-                <InputField label="Primary Contact No." name={"mother_primary_contact_no"} />
-                <InputField label="Secondary Contact No." name={"mother_secondary_contact_no"} />
+                <InputField label="First Name" name={"mother_first_name"} value={values?.mother_first_name} onChange={handleChange} error={errors.mother_first_name}/>
+                <InputField label="Middle Name" name={"mother_middle_name"} value={values?.mother_middle_name} onChange={handleChange} error={errors.mother_middle_name}/>
+                <InputField label="Last Name" name={"mother_last_name"} value={values?.mother_last_name} onChange={handleChange} error={errors.mother_last_name}/>
+                <InputField label="Email" name={"mother_email"} value={values?.mother_email} onChange={handleChange} error={errors.mother_email}/>
+                <InputField label="Date of Birth" type="date" name={"mother_dob"} value={values?.mother_dob} onChange={handleChange} error={errors.mother_dob}/>
+                <InputField label="Education" name={"mother_education"} value={values?.mother_education} onChange={handleChange} error={errors.mother_education}/>
+                <InputField label="Occupation" name={"mother_occupation"} value={values?.mother_occupation} onChange={handleChange} error={errors.mother_occupation}/>
+                <InputField label="organization" name={"mother_organization"} value={values?.mother_organization} onChange={handleChange} error={errors.mother_organization}/>
+                <InputField label="Designation" name={"mother_designation"} value={values?.mother_designation} onChange={handleChange} error={errors.mother_designation}/>
+                <InputField label="Annual Income" type="number" name={"mother_annual_income"} value={values?.mother_annual_income} onChange={handleChange} error={errors.mother_annual_income}/>
+                <InputField label="Primary Contact No." name={"mother_primary_contact_no"} value={values?.mother_primary_contact_no} onChange={handleChange} error={errors.mother_primary_contact_no}/>
+                <InputField label="Secondary Contact No." name={"mother_secondary_contact_no"} value={values?.mother_secondary_contact_no} onChange={handleChange} error={errors.mother_secondary_contact_no}/>
              
 
                 <h6 className="font-[600]">Local Guardian Details</h6>
                 <div></div>
                 <div></div>
                 <div></div>
-                <InputField label="First Name" name={"guardian_first_name"} />
-                <InputField label="Middle Name" name={"guardian_middle_name"} />
-                <InputField label="Last Name" name={"guardian_last_name"} />
-                <InputField label="Email" name={"guardian_email"} />
-                <InputField label="Relation" name={"guardian_relation"} />
-                <InputField label="Primary Contact No." name={"guardian_primary_contact_no"} />
-                <InputField label="Secondary Contact No." name={"guardian_secondary_contact_no"} />
+                <InputField label="First Name" name={"guardian_first_name"} value={values?.guardian_first_name} onChange={handleChange} error={errors.guardian_first_name}/>
+                <InputField label="Middle Name" name={"guardian_middle_name"} value={values?.guardian_middle_name} onChange={handleChange} error={errors.guardian_middle_name}/>
+                <InputField label="Last Name" name={"guardian_last_name"} value={values?.guardian_last_name} onChange={handleChange} error={errors.guardian_last_name}/>
+                <InputField label="Email" name={"guardian_email"} value={values?.guardian_email} onChange={handleChange} error={errors.guardian_email}/>
+                <InputField label="Relation" name={"guardian_relation"} value={values?.guardian_relation} onChange={handleChange} error={errors.guardian_relation}/>
+                <InputField label="Primary Contact No." name={"guardian_primary_contact_no"} value={values?.guardian_primary_contact_no} onChange={handleChange} error={errors.guardian_primary_contact_no}/>
+                <InputField label="Secondary Contact No." name={"guardian_secondary_contact_no"} value={values?.guardian_secondary_contact_no} onChange={handleChange} error={errors.guardian_secondary_contact_no}/>
 
                 <div></div>
                 <h6 className="font-[600]">Address</h6>
                 <div></div>
                 <div></div>
                 <div></div>
-                <InputField label="Address Line 1" name={"address1"} />
-                <InputField label="Address Line 2" name={"address2"} />
-                <InputField label="City" name={"city"} />
-                <InputField label="State" name={"state"} />
-                <InputField label="Country" name={"country"} />
-                <InputField label="PIN code" name={"pin_code"} />
+                <InputField label="Address Line 1" name={"address1"} value={values?.address1} onChange={handleChange} error={errors.address1}/>
+                <InputField label="Address Line 2" name={"address2"} value={values?.address2} onChange={handleChange} error={errors.address2}/>
+                <InputField label="City" name={"city"} value={values?.city} onChange={handleChange} error={errors.city}/>
+                <InputField label="State" name={"state"} value={values?.state} onChange={handleChange} error={errors.state}/>
+                <InputField label="Country" name={"country"} value={values?.country} onChange={handleChange} error={errors.country}/>
+                <InputField label="PIN code" name={"pin_code"} value={values?.pin_code} onChange={handleChange} error={errors.pin_code}/>
             </div>
         </form>
     );
